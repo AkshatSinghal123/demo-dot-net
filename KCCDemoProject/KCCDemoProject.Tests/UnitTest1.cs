9437e82c26370536e218fa51c8b2a00d9eb1ea29
Srivastava <Jaya.Srivastava@motherson.com>
2025-03-06 17:15:17 +0530

    added selenium tc

---
+++ KCCDemoProject/KCCDemoProject.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace KCCDemoProject.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true); // Basic test to ensure setup works
+        }
+    }
+}
